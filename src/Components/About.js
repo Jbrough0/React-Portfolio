7830b6abe92efe2ffaf1b665c80fdbf5173bea15
Josh Broughton <spikebroughton@aol.com>
2021-03-23 16:59:16 -0400

    add

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import annie from './annie.jpg';
+import annie from  './imgs/annie.jpg'
 export default class About extends Component {
   render() {
    let resumeData = this.props.resumeData;
+   
     return (<h1>My name is Josh Broughton
 
       I am an actor and editor and I am interested in audio engineering. I am a graduate of Butler High School and I recevied an associate in arts at CPCC in May 2019
       
       I am interested in coding and I am taking this class to become a better coder. Although I had some trouble with coding while taking an e-commerce class in high school, I believe that this bootcamp will give me a better understanding of coding and that I will be on my way to becoming a better coder after the bootcamp.</h1>
+      
       // <section id="about">
       //    <div className="row">
 
@@ -45,6 +47,8 @@ export default class About extends Component {
       //       </div>
       //    </div>
       // </section>
+      
     );
   }
 }
+<img src={annie} alt="picture with annie"/>
